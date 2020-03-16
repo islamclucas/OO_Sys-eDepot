88d412d07d49aa1fa802ac0c633c9ce55008f28b
islamclucas <46931234+islamclucas@users.noreply.github.com>
2020-03-16 17:11:52 +0000

    This is the start

---
+++ 5104-eDepot/src/ClassA.java
@@ -0,0 +1,12 @@
+
+public class ClassA {
+
+	
+	private void main() {
+		
+		System.out.println("hello welcome to the new project I hope this works");
+		System.out.println("xoxo gossip girl");
+		// TODO Auto-generated method stub
+
+	}
+}
