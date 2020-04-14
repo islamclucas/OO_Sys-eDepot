36f0b1a8354f022453e83f7bac4e818d66a78d8a
abiga <abiga@LAPTOP-6RJ9OOR6.lan>
2020-04-14 12:31:08 +0100

    -- planning outputs-- abbie

--- 5104-eDepot/src/depot/WorkSchedule.java
+++ 5104-eDepot/src/depot/WorkSchedule.java
@@ -6,12 +6,28 @@ public class WorkSchedule {
 	protected String client;
 	protected Date startDate;
 	protected Date endDate;
-	
-	public WorkSchedule (String client, Date startDate, Date endDate) {
+
+	public WorkSchedule(String client, Date startDate, Date endDate) {
 		this.client = client;
 		this.startDate = startDate;
 		this.endDate = endDate;
 	}
-	// test abbie
+
+	//view work schedule
+	//client name
+	//start date
+	//end date
+	//driver
+	//vehicle type
+	
+	
+	// arrange work schedule
+	// System.out.print.ln("--Work Schedule Setup--");
+	// System.out.print.ln("What is the name of the client?");
+	//	System.out.print.ln("What is the start date?");
+	//	System.out.print.ln("What is the end date?");
+	//	System.out.print.ln("Who is the driver?");
+	//	System.out.print.ln("What is the vehicle type?");
+
 
 }
