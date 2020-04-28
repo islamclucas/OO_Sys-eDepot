b9fdccf2de0d6fc15ae756f9cdf055de0a51959f
joeba <joeba@DESKTOP-JJBV9UO>
2020-04-28 14:59:19 +0100

    Entry fix

--- 5104-eDepot/src/Entry.java
+++ 5104-eDepot/src/Entry.java
@@ -1,6 +1,8 @@
+import java.io.IOException;
+import java.text.ParseException;
 
 public class Entry {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException, ParseException {
 		
 		new Sys().main(args);
 	}
